b975cb340a19e3cbcbc031ff6bd45dc622bc34af
Gilson Vieira <xlx.gilson.xlx@gmail.com>
2016-05-17 11:07:32 -0300

    Adicionada a tela principal, juntamente com as chamadas das outras telas (PGM1, PGM2 e PGM3)
    - TO DO:
     - Colorir a tela
     - Ajustar a exibição da data

--- MacKallCenter/Menu-Principal.cbl
+++ MacKallCenter/Menu-Principal.cbl
@@ -3,15 +3,89 @@
        environment division.
        configuration section.
 
-       data division.
-       working-storage section.
-       01 valido pic 9.
-       01 cpf pic 9(11) value 12312312312.
-       procedure division.
-
-       inicio.
-       
-           call "VALIDA-CPF" using valido, cpf
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 TMP-DATA.
+           05 ANO-TMP PIC X(4).
+           05 MES-TMP PIC XX.
+           05 DIA-TMP PIC XX.
+       01 W-DATA.
+           05 DIA PIC XX.
+           05 MES PIC XX.
+           05 ANO PIC XXXX.
+       77 OPC PIC 9 VALUE ZERO.
+           88 OPC-OK VALUES 1 THRU 4.
+       77 W-BRANCO PIC X(39).
+       SCREEN SECTION.
+       01  TELA.
+           05  LINE 01  COLUMN 01 
+               VALUE  "    ออออออออออออออออออออออออออออออออออออ".
+           05  LINE 01  COLUMN 41 
+               VALUE  "อออออออออออออออออออออออออออออออออออ".
+           05  LINE 03  COLUMN 01 
+               VALUE  "    TVMACK           MENU PRINCIPAL".
+           05  LINE 05  COLUMN 01 
+               VALUE  "    ออออออออออออออออออออออออออออออออออออ".
+           05  LINE 05  COLUMN 41 
+               VALUE  "อออออออออออออออออออออออออออออออออออ".
+           05  LINE 09  COLUMN 01 
+               VALUE  "                        1. CLIENTES".
+           05  LINE 10  COLUMN 01 
+               VALUE  "                        2. OCORRENCIAS".
+           05  LINE 11  COLUMN 01 
+               VALUE  "                        3. CONSULTAS".
+           05  LINE 12  COLUMN 01 
+               VALUE  "                        4. ENCERRA".
+           05  LINE 16  COLUMN 01 
+               VALUE  "    ออออออออออออออออออออออออออออออออออออ".
+           05  LINE 16  COLUMN 41 
+               VALUE  "อออออออออออออออออออออออออออออออออออ".
+           05  LINE 18  COLUMN 01 
+               VALUE  "    MENSAGENS: INFORME SUA OPCAO [ ]".
+           05  LINE 20  COLUMN 01 
+               VALUE  "    ออออออออออออออออออออออออออออออออออออ".
+           05  LINE 20  COLUMN 41 
+               VALUE  "อออออออออออออออออออออออออออออออออออ".
+           05  S-DATA
+               LINE 03  COLUMN 67  PIC XX/XX/XXXX
+               FROM  W-DATA.
+           05  S-OPC
+               LINE 18  COLUMN 35  PIC 9
+               TO     OPC.
            
-           display valido at 1010
-           stop run.
+       01 MENSAGENS.
+           05 MSG-BRANCO 
+           LINE 18 COLUMN 38 PIC X(39) FROM W-BRANCO.
+           05 MSG-OPC-INVALIDA
+           LINE 18 COLUMN 38 VALUE "OPCAO INVALIDA".
+       
+             
+       PROCEDURE DIVISION.
+       INICIO.
+           ROT-DATA.
+           DISPLAY TELA
+           
+           PERFORM WITH TEST AFTER UNTIL OPC EQUAL 4
+           ACCEPT S-OPC
+           IF NOT OPC-OK THEN
+           DISPLAY MSG-OPC-INVALIDA
+           STOP " "
+           DISPLAY MSG-BRANCO
+           ELSE
+           EVALUATE OPC
+           WHEN 1 CALL "PGM1"
+           WHEN 2 CALL "PGM2"
+           WHEN 3 CALL "PGM3"
+           END-EVALUATE
+           END-IF
+           END-PERFORM
+
+           
+           
+           STOP RUN.
+           
+       ROT-DATA.
+           ACCEPT W-DATA FROM DATE YYYYMMDD.
+           MOVE DIA-TMP TO DIA.
+           MOVE MES-TMP TO MES.
+           MOVE ANO-TMP TO ANO.

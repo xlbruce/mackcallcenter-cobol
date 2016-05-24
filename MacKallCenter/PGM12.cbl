7e0b7f8daa4baecfd1c0080e0c91cc7ebc09d55f
Gilson Vieira <xlx.gilson.xlx@gmail.com>
2016-05-24 10:56:36 -0300

    Tela de alteração de clientes adicionada

--- MacKallCenter/PGM12.cbl
+++ MacKallCenter/PGM12.cbl
@@ -37,59 +37,83 @@
        working-storage section.
        77 ST-CLIENTE PIC XX VALUE SPACES.
        77 RESP PIC X VALUE SPACE.
+       01 W-DATE.
+           05 DIA PIC 99.
+           05 MES PIC 99.
+           05 ANO PIC 9999.
+       01 LINHA.
+           05 FILLER VALUE "   ".
+           05 L PIC X(72) VALUE ALL "Í".
+           05 FILLER VALUE "    ".
+           
+       SCREEN SECTION.
+       01  TELA.
+           05 BLANK SCREEN.
+           05 LINE 01 COLUMN 01 PIC X(80)
+           FROM LINHA.
+           05 LINE 03 COLUMN 35 VALUE "TVMACK".
+           05 LINE 05 COLUMN 04 VALUE "ALTERACAO DE CLIENTE".
+           05 LINE 05 COLUMN 66 PIC 99/99/9999
+           FROM W-DATE.
+           05 LINE 07 COLUMN 01 PIC X(80)
+           FROM LINHA.
+           
+           05 LINE 09 COLUMN 04
+           VALUE "CPF DO CLIENTE.: [           ]".
+           05 S-CPF LINE 09 COLUMN 22 PIC X(11) TO CPF.
+           
+           05 LINE 10 COLUMN 04
+           VALUE "NOME DO CLIENTE: [".
+           05 LINE 10 COLUMN 34 VALUE "]".
+           
+           05 LINE 11 COLUMN 04
+           VALUE "ENDERECO.......: [".
+           05 LINE 11 COLUMN 49 VALUE "]".
+           
+           05 LINE 12 COLUMN 04
+           VALUE "COMPLEMENTO....: [          ]".
+           
+           05 LINE 13 COLUMN 04
+           VALUE "BAIRRO.........: [".
+           05 LINE 13 COLUMN 42 VALUE "]".
+           
+           05 LINE 14 COLUMN 04
+           VALUE "CIDADE.........: [".
+           05 LINE 14 COLUMN 42 VALUE "]".
+           
+           05 LINE 15 COLUMN 04
+           VALUE "ESTADO.........: [  ]".
+           
+           05 LINE 16 COLUMN 04
+           VALUE "CEP............: [     -   ]".
+           
+           05 LINE 17 COLUMN 04
+           VALUE "TELEFONE.......: [  ]-[    -    ]".
+           
+           05 LINE 18 COLUMN 04
+           VALUE "TIPO PACOTE....: [ ]-[          ]".
+           
+           05 LINE 19 COLUMN 04
+           VALUE "QTDE PONTOS....: [ ]".
+           
+           05 LINE 20 COLUMN 04
+           VALUE "DIA VENCIMENTO.: [  ]".
+           
+           05 LINE 21 COLUMN 04
+           VALUE "DATA INCLUSAO..: [  /  /    ]".
+           
+           05 LINE 22 COLUMN 04
+           VALUE "DATA ULT ALTERACAO [  /  /    ]".
+           
+           05 LINE 24 COLUMN 01 PIC X(80)
+           FROM LINHA.
+           05 LINE 25 COLUMN 05
+           VALUE "MENSAGENS: ".
+           05 LINE 26 COLUMN 01 PIC X(80)
+           FROM LINHA.
        
-       LINKAGE SECTION.
-       01 CPF-P PIC X(11).
-       01 NOME-P PIC X(30).
-       01 ENDERECO-P PIC X(45).
-       01 COMPLEMENTO-P PIC X(10).
-       01 BAIRRO-P PIC X(20).
-       01 CIDADE-P PIC X(20).
-       01 ESTADO-P PIC X(2).
-       01 CEP-P PIC X(9).
-       01 DDD-P PIC 9(2).
-       01 TELEFONE-P PIC X(9).
-       01 DATA-INCLUSAO-P PIC X(10).
-       01 TIPO-PACOTE-P PIC X(1).
-       01 QTDE-PONTOS-RESIDENCIA-P PIC 9(1).
-       01 VENCIMENTO-FATURA-P PIC 9(2).
-       01 DATA-INCLUSAO-CLIENTE-P PIC X(10).
-       01 DATA-ULTIMA-ALTERACAO-P PIC X(10).
-       01 RETORNO PIC 9.
-       procedure division 
-           USING 
-           CPF-P,NOME-P,ENDERECO-P,COMPLEMENTO-P,BAIRRO-P,CIDADE-P,
-           ESTADO-P,CEP-P,DDD-P,TELEFONE-P,DATA-INCLUSAO-P,TIPO-PACOTE-P
-           QTDE-PONTOS-RESIDENCIA-P,VENCIMENTO-FATURA-P,                
-           DATA-INCLUSAO-CLIENTE-P,DATA-ULTIMA-ALTERACAO-P,RETORNO.     
+       PROCEDURE DIVISION.   
        INICIO.
-           
-           OPEN I-O ARQ-CLIENTES
-           
-           READ ARQ-CLIENTES
-               MOVE CPF-P TO CPF
-               IF ST-CLIENTE = "23"
-                   MOVE 0 TO RETORNO
-               ELSE 
-                   MOVE NOME-P TO NOME
-                   MOVE ENDERECO-P TO ENDERECO
-                   MOVE COMPLEMENTO-P TO COMPLEMENTO
-                   MOVE BAIRRO-P TO BAIRRO
-                   MOVE CIDADE-P TO CIDADE
-                   MOVE ESTADO-P TO ESTADO
-                   MOVE CEP-P TO CEP
-                   MOVE DDD-P TO DDD
-                   MOVE TELEFONE-P TO TELEFONE
-                   MOVE DATA-INCLUSAO-P TO DATA-INCLUSAO
-                   MOVE TIPO-PACOTE-P TO TIPO-PACOTE
-                   MOVE QTDE-PONTOS-RESIDENCIA-P TO 
-                   QTDE-PONTOS-RESIDENCIA
-                   MOVE VENCIMENTO-FATURA-P TO VENCIMENTO-FATURA
-                   MOVE DATA-INCLUSAO-CLIENTE-P TO DATA-INCLUSAO-CLIENTE
-                   MOVE DATA-ULTIMA-ALTERACAO-P TO DATA-ULTIMA-ALTERACAO
-                   MOVE 1 TO RETORNO
-                   REWRITE REG-CLIENTE
-               END-IF
-               
-           CLOSE ARQ-CLIENTES
+           DISPLAY TELA.
+      *TODO ler o arquivo de clientes, e preencher a tela 
        EXIT program

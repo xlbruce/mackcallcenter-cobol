d0af0cb1e3d6370741b542152c5d02e1bb36b5e0
Bruno Faganello <bruno.faganello.neto@gmail.com>
2016-05-24 11:23:47 -0300

    Logica da tela concluída e testada

--- MacKallCenter/PGM11.cbl
+++ MacKallCenter/PGM11.cbl
@@ -42,7 +42,7 @@
        77 ST-CLIENTE PIC XX VALUE SPACES.
        77 RESP PIC X VALUE SPACE.
        01 LINHA.
-           05 FILLER VALUE "    ".
+           05 FILLER VALUE "   ".
            05 L PIC X(72) VALUE ALL "Í".
            05 FILLER VALUE "    ".
            
@@ -51,10 +51,10 @@
            05 MES PIC 99.
            05 ANO PIC 9999.
        01 CLIENTE-AUX.
-           05 CEP.
+           05 CEP-CLI-AUX.
                10 CEP1 PIC 9(5).
                10 CEP2 PIC 999.
-           05 TELEFONE.
+           05 TELEFONE-CLI-AUX.
                10 TEL1 PIC 99.
                10 TEL2 PIC 9(8).
            05 TIPO.
@@ -75,60 +75,109 @@
            
            05 LINE 09 COLUMN 04
            VALUE "CPF DO CLIENTE.: [           ]".
+           05 S-CPF LINE 09 COLUMN 22 PIC X(11) TO CPF.
            
            05 LINE 10 COLUMN 04
            VALUE "NOME DO CLIENTE: [".
            05 LINE 10 COLUMN 34 VALUE "]".
+           05 S-NOME LINE 10 COLUMN 22 PIC X(30) TO NOME.
            
            05 LINE 11 COLUMN 04
            VALUE "ENDERECO.......: [".
            05 LINE 11 COLUMN 49 VALUE "]".
+           05 S-ENDERECO LINE 11 COLUMN 22 PIC X(45) TO ENDERECO.
            
            05 LINE 12 COLUMN 04
            VALUE "COMPLEMENTO....: [          ]".
+           05 S-COMPLEMENTO LINE 12 COLUMN 22 PIC X(10) TO COMPLEMENTO.
            
            05 LINE 13 COLUMN 04
            VALUE "BAIRRO.........: [".
            05 LINE 13 COLUMN 42 VALUE "]".
+           05 S-BAIRRO LINE 13 COLUMN 22 PIC X(20) TO BAIRRO.
            
            05 LINE 14 COLUMN 04
            VALUE "CIDADE.........: [".
            05 LINE 14 COLUMN 42 VALUE "]".
+           05 S-CIDADE LINE 14 COLUMN 22 PIC X(20) TO CIDADE.
            
            05 LINE 15 COLUMN 04
            VALUE "ESTADO.........: [  ]".
+           05 S-ESTADO LINE 15 COLUMN 22 PIC XX TO ESTADO.
            
            05 LINE 16 COLUMN 04
            VALUE "CEP............: [     -   ]".
+           05 S-CEP LINE 16 COLUMN 22  PIC X(8) TO CEP.
            
            05 LINE 17 COLUMN 04
            VALUE "TELEFONE.......: [  ]-[    -    ]".
+           05 S-TELEFONE LINE 17 COLUMN 22 PIC X(10) TO TELEFONE.
            
            05 LINE 18 COLUMN 04
-           VALUE "TIPO PACOTE....: [ ]-[          ]".
+           VALUE "TIPO PACOTE....: [ ]".
+           05 S-TIPO LINE 18 COLUMN 22 PIC X TO TIPO-PACOTE.
            
            05 LINE 19 COLUMN 04
            VALUE "QTDE PONTOS....: [ ]".
+           05 S-PONTOS LINE 19 COLUMN 22 PIC X TO 
+           QTDE-PONTOS-RESIDENCIA.
            
            05 LINE 20 COLUMN 04
            VALUE "DIA VENCIMENTO.: [  ]".
+           05 S-VENCIMENTO LINE 20 COLUMN 22 PIC XX TO 
+           VENCIMENTO-FATURA.
            
            05 LINE 21 COLUMN 04
-           VALUE "DATA INCLUSAO..: [  /  /    ]".
+           VALUE "DATA INCLUSAO..: [ / /   ]".
+           05 S-DT-INCLUSAO.
+               10 S-DT-DIA LINE 21 COLUMN 22 PIC 99 .
+               10 S-DT-MES LINE 21 COLUMN 24 PIC 99.
+               10 S-DT-ANO LINE 21 COLUMN 26 PIC 9999.
            
            05 LINE 22 COLUMN 04
            VALUE "DATA ULT ALTERACAO [  /  /    ]".
-           
+           05 S-UL-INCLUSAO.
+               10 S-UL-DIA LINE 21 COLUMN 22 PIC 99 .
+               10 S-UL-MES LINE 21 COLUMN 24 PIC 99.
+               10 S-UL-ANO LINE 21 COLUMN 26 PIC 9999.
            05 LINE 24 COLUMN 01 PIC X(80)
            FROM LINHA.
            05 LINE 25 COLUMN 05
            VALUE "MENSAGENS: ".
+            05 s-Sucesso line 25 column 15 value " ".
            05 LINE 26 COLUMN 01 PIC X(80)
            FROM LINHA.
            
        PROCEDURE DIVISION.
        INICIO.
            DISPLAY TELA.    
+           accept S-CPF
+           ACCEPT S-NOME
+           ACCEPT S-ENDERECO
+           ACCEPT S-COMPLEMENTO
+           ACCEPT S-BAIRRO
+           ACCEPT S-CIDADE
+           ACCEPT S-ESTADO
+           ACCEPT S-CEP
+           ACCEPT S-TELEFONE
+           ACCEPT S-TIPO
+           ACCEPT S-PONTOS
+           ACCEPT S-VENCIMENTO
+            OPEN I-O ARQ-CLIENTES
+               READ ARQ-CLIENTES
+           
+           
+           
+               IF ST-CLIENTE = "23"
+                   WRITE REG-CLIENTE
+                   DISPLAY S-SUCESSO
+               ELSE 
+                   
+               END-IF
+               
+           
+           
+           
            STOP RUN.
       *TODO - FINISH SCREEN
        EXIT program 
